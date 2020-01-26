44df9a10c6f86eaef4fa3c58d0c2dc6668d50393
ppoocoder <youngwoo5880@likelion.org>
2020-01-27 00:51:33 +0900

    프로젝트 셋팅 및 routes 설정

---
+++ src/routes/index.js
@@ -0,0 +1,40 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import NewsView from '../views/NewsView.vue';
+import AskView from '../views/AskView.vue';
+import JobsView from '../views/JobsView.vue';
+import UserView from '../views/UserView.vue';
+import ItemView from '../views/ItemView.vue';
+Vue.use(VueRouter);
+
+export const router = new VueRouter({
+  routes: [
+    {
+      path: '/',
+      redirect: '/news',
+  },
+   {
+       path: '/news',
+       component: NewsView,
+   },
+   {   //path: url 주소
+       path:'/ask',
+       //component: url 주로 갔을 때 표시될 컴포넌트
+       component:AskView,
+   },
+   {
+    path: '/job',
+    component: JobsView,
+    },
+
+    {
+      path: '/user',
+      component: UserView,
+      },
+      // {
+      //   path: '/items',
+      //   component: JobsView,
+      //   },
+
+  ]
+});
